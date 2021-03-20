ff9de52602b915ef4e9c4d32e58d1569fc65261a
mnb27 <2018csb1069@iitrpr.ac.in>
2021-03-20 15:28:02 +0530

    nav dashboard added

--- app/src/main/java/com/example/cfb/MainActivity.kt
+++ app/src/main/java/com/example/cfb/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.cfb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val login: CardView = findViewById(R.id.button_login)
+        val signup: CardView = findViewById(R.id.button_signup)
+
+        login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        signup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
