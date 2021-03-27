d2735dd7a6a2ba8d8c3c84a685ddc3fc7c9db498
mnb27 <2018csb1069@iitrpr.ac.in>
2021-03-27 12:15:57 +0530

    added mobile OTP verification

--- app/src/main/java/com/example/cfb/MainActivity.kt
+++ app/src/main/java/com/example/cfb/MainActivity.kt
@@ -3,26 +3,35 @@ package com.example.cfb
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.cardview.widget.CardView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val login: CardView = findViewById(R.id.button_login)
-        val signup: CardView = findViewById(R.id.button_signup)
+        auth= FirebaseAuth.getInstance()
+        var currentUser=auth.currentUser
 
-        login.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+//        Reference
+        val logout=findViewById<Button>(R.id.idLogout)
+
+        if(currentUser==null){
+            startActivity(Intent(this,LoginActivity::class.java))
             finish()
         }
-        signup.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+
+        logout.setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this,LoginActivity::class.java))
             finish()
         }
 
     }
 }
+
